2d0b9b1329704e43499c7fb6410e681e1f1c169d
Erick <eeramirez@tuvansa.com.mx>
2022-10-10 19:10:21 -0500

    Se agrega url de produccion backend

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -1,6 +1,6 @@
 export const fetchAPI = ({endpoint, data, method, signal }) => {
 
-    const url = `http://localhost:3000/api/${endpoint}`;
+    const url = `https://encuestas-back-end.herokuapp.com/api/${endpoint}`;
 
     if (method === 'GET') {
         return fetch(url)
